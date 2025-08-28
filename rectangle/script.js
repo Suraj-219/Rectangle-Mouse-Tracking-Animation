d7a46497c6c61d4d0274e7f8705c900a746f753b
Suraj Pal <surajpal7442@gmail.com>
2025-08-28 08:37:11 +0530

    Update script.js

--- rectangle/script.js
+++ rectangle/script.js
@@ -1,11 +1,29 @@
 window.addEventListener("mousemove", function(details){
-    var rect = this.document.querySelector("#rect");
+    var rect = document.querySelector("#rect");
 
-    var xval = gsap.utils.mapRange(0, window.innerWidth, 100 + rect.getBoundingClientRect().width/2,
-       window.innerWidth - (100 + rect.getBoundingClientRect().width/2), details.clientX);
+    var xval = gsap.utils.mapRange(
+        0, window.innerWidth,
+        100 + rect.getBoundingClientRect().width / 2,
+        window.innerWidth - (100 + rect.getBoundingClientRect().width / 2),
+        details.clientX
+    );
 
-    gsap.to('#rect',{
+    var yval = gsap.utils.mapRange(
+        0, window.innerHeight,
+        100 + rect.getBoundingClientRect().height / 2,
+        window.innerHeight - (100 + rect.getBoundingClientRect().height / 2),
+        details.clientY
+    );
+
+    var rotateX = gsap.utils.mapRange(0, window.innerHeight, 15, -15, details.clientY);
+    var rotateY = gsap.utils.mapRange(0, window.innerWidth, -15, 15, details.clientX);
+
+    gsap.to('#rect', {
         left: xval,
-        ease: Power3
+        top: yval,
+        rotateX: rotateX,
+        rotateY: rotateY,
+        ease: "power3.out",
+        duration: 0.5
     });
 });
